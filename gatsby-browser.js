29f70e111b5dd8fd9ae801035daaf7a028e2a84d
Philip <bizimisphilip@gmail.com>
2021-06-22 15:16:27 +0200

    Add basic Auth0 login/logout logic

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,21 @@
 import './src/styles/global.css';
+import React from 'react';
+import { Auth0Provider } from '@auth0/auth0-react';
+import { navigate } from 'gatsby';
+
+const onRedirectCallback = (appState) => navigate(appState?.returnTo || '/');
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <Auth0Provider
+      domain={process.env.GATSBY_DOMAIN}
+      clientId={process.env.GATSBY_CLIENT_ID}
+      audience={process.env.GATSBY_AUDIENCE}
+      scope="read:users"
+      redirectUri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {element}
+    </Auth0Provider>
+  );
+};
