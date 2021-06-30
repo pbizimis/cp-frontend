ff37f9561a64079f6705f9d76a795f4d4849d13d
Philip <bizimisphilip@gmail.com>
2021-06-30 15:34:17 +0200

    Change navbar style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,7 +18,7 @@ export function Nav() {
 <Popover as="header" className="relative">
           {({ open }) => (
             <>
-              <div className="bg-gray-900 pt-6">
+              <div className="bg-gray-900 py-3">
                 <nav
                   className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
                   aria-label="Global"
