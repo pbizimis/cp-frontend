efb796f3f2c8dd83e5d8ec6d6d8b7d9fbe897e16
Philip <bizimisphilip@gmail.com>
2021-07-28 21:28:27 +0200

    Change nav bar to a single console and dot nav menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,161 +1,112 @@
-import React from 'react';
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
-import { useAuth0 } from '@auth0/auth0-react';
-import { Link } from "gatsby";
+import React from "react"
+import { Fragment } from "react"
+import { Transition, Menu } from "@headlessui/react"
+import { useAuth0 } from "@auth0/auth0-react"
+import { Link } from "gatsby"
 
-const navigation = [
-  { name: 'Features', href: '/' },
-  { name: 'Console', href: '/console' },
-]
+import { DotsVerticalIcon } from "@heroicons/react/solid"
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ")
+}
+
+function DotMenu({ logout, name }) {
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      {({ open }) => (
+        <>
+          <div>
+            <Menu.Button className="bg-gray-900 rounded-full flex items-center text-white hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+              <span className="sr-only">Open options</span>
+              <DotsVerticalIcon className="h-6 w-6" aria-hidden="true" />
+            </Menu.Button>
+          </div>
+
+          <Transition
+            show={open}
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items
+              static
+              className="origin-top-right absolute z-50 right-0 mt-4 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+            >
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <h1
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm w-full"
+                      )}
+                    >
+                      Hi, {name}
+                    </h1>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                        "block px-4 py-2 text-sm cursor-pointer w-full text-left"
+                      )}
+                      id="logout"
+                      onClick={() =>
+                        logout({ returnTo: window.location.origin })
+                      }
+                    >
+                      Log Out
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
+    </Menu>
+  )
+}
 
 export function Nav() {
-    
-    const { isAuthenticated, user, loginWithRedirect, logout } = useAuth0();
+  const { isAuthenticated, user, logout } = useAuth0()
 
   return (
-<Popover as="header" className="relative">
-          {({ open }) => (
-            <>
-              <div className="bg-gray-900 py-3">
-                <nav
-                  className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
-                  aria-label="Global"
-                >
-                  <div className="flex items-center flex-1">
-                    <div className="flex items-center justify-between w-full md:w-auto">
-                      <Link to="/">
-                        <span className="sr-only">Workflow</span>
-                        <img
-                          className="h-8 w-auto sm:h-10"
-                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                          alt=""
-                        />
-                      </Link>
-                      <div className="-mr-2 flex items-center md:hidden">
-                        <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
-                          <span className="sr-only">Open main menu</span>
-                          <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                        </Popover.Button>
-                      </div>
-                    </div>
-                    <div className="hidden space-x-8 md:flex md:ml-10">
-                      {navigation.map((item) => (
-                        <Link
-                          to={item.href}
-                          className="text-base font-medium text-white hover:text-gray-300"
-                        >
-                          {item.name}
-                        </Link>
-                      ))}
-                    </div>
-                  </div>
-                  {isAuthenticated ? (
-                        
-                        <div className="hidden md:flex md:items-center md:space-x-6">
-                        <p className="text-base font-medium text-white hover:text-gray-300">
-                            {user.name}
-                        </p>
-                        <button
-                            className="text-base font-medium text-white hover:text-gray-300"
-                            id="logout"
-            onClick={() => logout({ returnTo: window.location.origin })}
-                            >
-                            Log Out
-                        </button>
-                        <h1
-                        href="#"
-                        className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-                        >
-                        Your Credits: 
-                        </h1>
-                        </div>
-                    ) : (
-                        <div className="hidden md:flex md:items-center md:space-x-6">
-                        <button
-                            className="text-base font-medium text-white hover:text-gray-300"
-                            id="login"
-                            onClick={() => loginWithRedirect()}
-                        >
-                            Log In
-                        </button>
-                        <a
-                          href="#"
-                          className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
-                        >
-                          Get credits
-                        </a>
-                      </div>
-                    )}
-                </nav>
-              </div>
-
-              <Transition
-                show={open}
-                as={Fragment}
-                enter="duration-150 ease-out"
-                enterFrom="opacity-0 scale-95"
-                enterTo="opacity-100 scale-100"
-                leave="duration-100 ease-in"
-                leaveFrom="opacity-100 scale-100"
-                leaveTo="opacity-0 scale-95"
-              >
-                <Popover.Panel
-                  focus
-                  static
-                  className="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
-                >
-                  <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                    <div className="px-5 pt-4 flex items-center justify-between">
-                      <div>
-                        <img
-                          className="h-8 w-auto"
-                          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                          alt=""
-                        />
-                      </div>
-                      <div className="-mr-2">
-                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-                          <span className="sr-only">Close menu</span>
-                          <XIcon className="h-6 w-6" aria-hidden="true" />
-                        </Popover.Button>
-                      </div>
-                    </div>
-                    <div className="pt-5 pb-6">
-                      <div className="px-2 space-y-1">
-                        {navigation.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
-                          >
-                            {item.name}
-                          </a>
-                        ))}
-                      </div>
-                      <div className="mt-6 px-5">
-                        <a
-                          href="#"
-                          className="block text-center w-full py-3 px-4 rounded-md shadow bg-indigo-600 text-white font-medium hover:bg-indigo-700"
-                        >
-                          Start free trial
-                        </a>
-                      </div>
-                      <div className="mt-6 px-5">
-                        <p className="text-center text-base font-medium text-gray-500">
-                          Existing customer?{' '}
-                          <a href="#" className="text-gray-900 hover:underline">
-                            Login
-                          </a>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Popover.Panel>
-              </Transition>
-            </>
-          )}
-        </Popover>
-    );
+    <div className="relative z-50 bg-gray-900 py-3">
+      <nav
+        className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+        aria-label="Global"
+      >
+        <div className="flex items-center flex-1">
+          <div className="flex items-center justify-between w-full md:w-auto">
+            <Link to="/">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
+                alt=""
+              />
+            </Link>
+          </div>
+        </div>
+        <Link
+          to="/console"
+          className="inline-flex items-center px-3 py-1 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+        >
+          Console
+        </Link>
+        {isAuthenticated && (
+          <div className="flex items-center ml-6">
+            <DotMenu logout={logout} name={user.name} />
+          </div>
+        )}
+      </nav>
+    </div>
+  )
 }
