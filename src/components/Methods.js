b45f982327f70a0045ebf5bb30d04548be6732d9
Philip <bizimisphilip@gmail.com>
2021-08-09 00:16:02 +0200

    Change loading circle

--- src/components/Methods.js
+++ src/components/Methods.js
@@ -193,7 +193,27 @@ export function Methods({ getAccessTokenSilently, data, url_prefix }) {
       methods.push(data[method])
     }
   } else {
-    return <Loading />
+    return (
+      <div className="flex justify-center items-center mt-24">
+        <div className="flex items-center justify-center w-full h-full">
+          <div className="flex justify-center items-center text-gray-300">
+            <svg
+              fill="none"
+              className="w-24 h-24 animate-spin"
+              viewBox="0 0 32 32"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                clipRule="evenodd"
+                d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+                fill="currentColor"
+                fillRule="evenodd"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return (
