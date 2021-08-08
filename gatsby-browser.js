41f656b542ef62d5cb45ff4ee966e8c7216642b9
Philip <bizimisphilip@gmail.com>
2021-08-09 00:44:43 +0200

    Add refresh tokens for cross browser functionality

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -13,6 +13,7 @@ export const wrapRootElement = ({ element }) => {
       audience={process.env.GATSBY_AUDIENCE}
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
+      useRefreshTokens={true}
     >
       {element}
     </Auth0Provider>
