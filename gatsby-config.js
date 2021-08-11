a7f8d9d6725764addf77a3b8ec3aaa75f0a80f68
Philip <bizimisphilip@gmail.com>
2021-08-12 01:34:58 +0200

    Add csp headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,21 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: ['gatsby-plugin-postcss'],
+  plugins: [
+    "gatsby-plugin-postcss",
+    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false,
+        mergeScriptHashes: false,
+        mergeStyleHashes: false,
+        mergeDefaultDirectives: false,
+        directives: {
+          "default-src": "'self' data: blob: 'unsafe-inline' 'unsafe-eval' https://webdesigan.com https://auth.webdesigan.com https://images.webdesigan.com https://api.webdesigan.com",
+        }
+      }
+    }
+  ],
 }
